a708e0d1d9508c4619e6303f3b2f633dbfe1eba2
ozzyozbourne <khanosaid726@gmail.com>
2024-05-26 00:42:44 -0400

    south of heaven

--- quarkus_in_action/code-with-quarkus/src/main/java/org/acme/GreetingResource.java
+++ quarkus_in_action/code-with-quarkus/src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,40 @@
 package org.acme;
 
+import java.security.Principal;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.SecurityContext;
 
 @Path("/hello")
 public class GreetingResource {
 
+    @ConfigProperty(name = "greeting")
+    String greeting;
+
+    @ConfigProperty(name = "greeting.test.new")
+    String newGreeting;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST";
+        return newGreeting;
+    }
+
+    @GET
+    @Path("/whoami")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String whoAmI(@Context SecurityContext securityContext) {
+        Principal principal = securityContext.getUserPrincipal();
+        if (principal != null) {
+            return principal.getName();
+        } else {
+            return "anonymous";
+        }
     }
 }
